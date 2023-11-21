14ae07c112f2339b90c25508cc3eb70d20bc2462
Zixian Cai <u5937495@anu.edu.au>
2023-11-21 02:09:59 +0000

    Conditional compile m5

--- build.rs
+++ build.rs
@@ -4,11 +4,13 @@ use std::path::Path;
 
 fn main() -> Result<()> {
     prost_build::compile_protos(&["./src/heapdump.proto"], &["./src"])?;
-    let root = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-lib=static=m5");
-    println!(
-        "cargo:rustc-link-search=native={}",
-        Path::new(&root).join("vendor/m5").to_str().unwrap()
-    );
+    if cfg!(feature = "m5") {
+        let root = env::var("CARGO_MANIFEST_DIR").unwrap();
+        println!("cargo:rustc-link-lib=static=m5");
+        println!(
+            "cargo:rustc-link-search=native={}",
+            Path::new(&root).join("vendor/m5").to_str().unwrap()
+        );
+    }
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate lazy_static;
 extern crate log;
 
 mod heapdump;
+#[cfg(feature = "m5")]
 mod m5;
 mod mark;
 mod sanity;
