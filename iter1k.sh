6d74777feddc308894ca38a7a7bbf2143a5e7fdf
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2024-02-23 15:23:22 +1100

    Add bench script

---
+++ iter1k.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export RUST_LOG=trace
+
+features=detailed_stats
+object_model=OpenJDK
+
+# snapshop_files=./sampled/fop/heapdump.2.binpb.zst
+snapshop_files=./sampled/pmd/heapdump.33.binpb.zst
+
+cargo run --release --features $features -- $snapshop_files --object-model $object_model trace --tracing-loop EdgeSlot -i 1000 &> edgeslot.log
+cargo run --release --features $features -- $snapshop_files --object-model $object_model trace --tracing-loop WP -i 1000 &> crossbeam.log
+cargo run --release --features $features -- $snapshop_files --object-model $object_model trace --tracing-loop WPMMTk -i 1000 &> mmtkwp.log
