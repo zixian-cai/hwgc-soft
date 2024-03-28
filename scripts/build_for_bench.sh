80c4d4212b743c4e3aab15fe54f32c1554017e6e
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2024-03-28 17:39:40 +1100

    wip

--- scripts/build_for_bench.sh
+++ scripts/build_for_bench.sh
@@ -6,10 +6,10 @@ rm -rf ./builds
 
 mkdir -p ./builds
 
-cargo build --release --features forwarding
+cargo build --release
 
 cp target/release/hwgc_soft ./builds/all_in_one
 
-cargo build --release --features forwarding,no_space_dispatch
+cargo build --release --features fifo
 
-cp target/release/hwgc_soft ./builds/all_in_one_no_space_dispatch
+cp target/release/hwgc_soft ./builds/all_in_one_fifo
