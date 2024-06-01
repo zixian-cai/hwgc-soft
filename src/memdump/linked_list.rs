bba8e089e9d2ae62dd9deb1679a0c2dd61f386eb
Zixian Cai <u5937495@anu.edu.au>
2024-06-01 09:50:21 +0000

    Rewrite the synthetic linked list workload using the much saner interface

--- src/memdump/linked_list.rs
+++ src/memdump/linked_list.rs
@@ -1,3 +1,5 @@
+use crate::MemoryInterface;
+
 use super::MemdumpWorkload;
 
 ///
@@ -14,38 +16,29 @@ impl LinkedList {
 impl MemdumpWorkload for LinkedList {
     unsafe fn gen_memdump(&self, md: &mut super::Memdump) {
         info!("Synthesized memdump of a linked list");
-        let word_size = 8_usize;
-        // 1 root slot plus 1 word for the number of root slots
-        let roots_size = word_size + 1;
-        let segment_roots = md.alloc_segment(roots_size);
-        let num_root_ptr = segment_roots.start as *mut usize;
-        info!(
-            "The number of roots is stored at {:?}",
-            md.translate_to_target(num_root_ptr)
-        );
-        let root_slot_ptr = num_root_ptr.wrapping_add(1);
-        info!(
-            "Roots are stored from {:?}",
-            md.translate_to_target(root_slot_ptr)
-        );
-        num_root_ptr.write(1);
-        // a next pointer plus a valuel
-        let node_size = word_size * 2;
-        let num_nodes = 1024;
-        let segment_nodes = md.alloc_segment(node_size * self.num_nodes);
-        info!(
-            "Generating {} linked list nodes stored from {:?}",
-            num_nodes,
-            md.translate_to_target(segment_nodes.start)
-        );
-        root_slot_ptr.write(md.translate_to_target(segment_nodes.start) as _);
-        let mut cursor = segment_nodes.start as *mut usize;
+        // The address space of the linked list looks like
+        // 0x0: number of root pointers, which is one
+        // 0x8: the signular pointer to the start of the linked list
+        // 0x1000: stores the linked list nodes
+        // ...
+        // 0x1000 + num_nodes * 16
+        // A total of 2 mappings needs to be allocated
+        let num_nodes = self.num_nodes;
+        md.new_mapping(0 as *mut u8, 16);
+        md.new_mapping(0x1000 as *mut u8, 16 * self.num_nodes);
+        let memif = md.gen_memif();
+        memif.write_value_to_target(0 as *mut u64, 1);
+        memif.write_pointer_to_target(0x8 as *mut *const u64, 0x1000 as *const u64);
+
         for i in 0..num_nodes {
+            // a next pointer and then a value
             if i != num_nodes - 1 {
-                cursor.write(md.translate_to_target(cursor.wrapping_add(2)) as _);
+                memif.write_pointer_to_target(
+                    (0x1000 + 16 * i) as *mut *const u64,
+                    (0x1000 + 16 * (i + 1)) as *const u64,
+                );
             }
-            cursor.wrapping_add(1).write(i);
-            cursor = cursor.wrapping_add(2);
+            memif.write_value_to_target((0x1000 + 16 * i + 8) as *mut u64, i as u64);
         }
     }
 }
--- src/memdump/mod.rs
+++ src/memdump/mod.rs
@@ -1,58 +1,128 @@
 use std::fs;
 
+use crate::MemoryInterface;
 use crate::*;
 use anyhow::Result;
 
 mod linked_list;
 
+/// Memory dump for making heapdumps usable on devices with physical memory only
+///
+/// It manages a chunk of host memory backing the device/target address space,
+/// which will be later be written to disk
+///
+/// It also maintains a translation between heapdump/host (or other synthetic
+/// workload)'s address space and the target address space (and the
+/// corresponding backing memory)
 #[derive(Debug)]
 struct Memdump {
-    start: *mut u8,
-    size: usize,
-    segments: Vec<MemdumpSegment>,
-    cursor: *mut u8,
-    mem_base: *mut u8,
+    backing_memory: *mut u8,
+    backing_memory_size: usize,
+    backing_memory_cursor: *mut u8,
+    target_memory_base: *mut u8,
+    mappings: Vec<MemdumpMapping>,
 }
 
 #[derive(Debug, Clone, Copy)]
-struct MemdumpSegment {
-    start: *mut u8,
+struct MemdumpMapping {
+    backing_memory_start: *mut u8,
+    target_memory_start: *mut u8,
+    host_memory_start: *mut u8,
     size: usize,
 }
 
+struct MemdumpMemoryInterface {
+    sorted_mappings: Vec<MemdumpMapping>,
+}
+
 impl Memdump {
-    fn new(size: usize, mem_base: *mut u8) -> Self {
-        assert_eq!(size % 4096, 0, "Memdump size must be mulitples of 4KB");
-        let start = crate::util::mmap_anon(size).unwrap() as *mut u8;
-        Memdump {
-            start,
-            size,
-            segments: vec![],
-            cursor: start,
-            mem_base,
+    fn gen_memif(&self) -> MemdumpMemoryInterface {
+        let mut mappings = self.mappings.clone();
+        mappings.sort_by_key(|x| x.host_memory_start);
+        MemdumpMemoryInterface {
+            sorted_mappings: mappings,
+        }
+    }
+}
+
+impl MemdumpMemoryInterface {
+    fn translate<T, F>(&self, ptr_host: *const T, key: F) -> *mut u8
+    where
+        F: Fn(&MemdumpMapping) -> *mut u8,
+    {
+        let r = self
+            .sorted_mappings
+            .binary_search_by_key(&(ptr_host as *mut u8), |x| x.host_memory_start);
+        match r {
+            Ok(x) => key(&self.sorted_mappings[x]),
+            Err(x) => unsafe {
+                debug_assert!(x > 0);
+                let mapping = &self.sorted_mappings[x - 1];
+                let host_offset = ptr_host.byte_offset_from(mapping.host_memory_start);
+                debug_assert!(host_offset > 0 && host_offset < mapping.size as isize);
+                key(mapping).byte_offset(host_offset)
+            },
         }
     }
 
-    fn alloc_segment(&mut self, size: usize) -> MemdumpSegment {
+    fn translate_to_target<T>(&self, ptr_host: *const T) -> *mut u8 {
+        self.translate(ptr_host, |m| m.target_memory_start)
+    }
+
+    fn translate_to_backing<T>(&self, ptr_host: *const T) -> *mut u8 {
+        self.translate(ptr_host, |m| m.backing_memory_start)
+    }
+}
+
+impl MemoryInterface for MemdumpMemoryInterface {
+    unsafe fn write_pointer_to_target<T>(&self, dst_host: *mut *const T, src_host: *const T) {
+        let dst_backing = self.translate_to_backing(dst_host);
+        let src_target = self.translate_to_target(src_host);
+        std::ptr::write(dst_backing as *mut *const T, src_target as *const T);
+    }
+
+    unsafe fn write_value_to_target<T>(&self, dst_host: *mut T, src: T) {
+        let dst_backing = self.translate_to_backing(dst_host);
+        std::ptr::write(dst_backing as *mut T, src);
+    }
+}
+
+impl Memdump {
+    fn new(size: usize, target_memory_base: *mut u8) -> Self {
+        assert_eq!(size % 4096, 0, "Memdump size must be mulitples of 4KB");
+        let backing_memory = crate::util::mmap_anon(size).unwrap() as *mut u8;
+        Memdump {
+            backing_memory,
+            backing_memory_size: size,
+            backing_memory_cursor: backing_memory,
+            target_memory_base,
+            mappings: vec![],
+        }
+    }
+
+    fn new_mapping(&mut self, host_memory_start: *mut u8, size: usize) -> MemdumpMapping {
         let size_aligned = crate::util::align_up(size, 4096);
-        let ret = self.cursor;
-        let new_cursor = self.cursor.wrapping_add(size_aligned);
-        assert!(new_cursor <= self.start.wrapping_add(self.size));
-        self.cursor = new_cursor;
-        MemdumpSegment { start: ret, size }
+        let old_cursor = self.backing_memory_cursor;
+        let new_cursor = self.backing_memory_cursor.wrapping_add(size_aligned);
+        assert!(new_cursor <= self.backing_memory.wrapping_add(self.backing_memory_size));
+        self.backing_memory_cursor = new_cursor;
+        let ret = MemdumpMapping {
+            backing_memory_start: old_cursor,
+            target_memory_start: unsafe {
+                self.target_memory_base
+                    .byte_offset(old_cursor.offset_from(self.backing_memory))
+            },
+            host_memory_start,
+            size,
+        };
+        self.mappings.push(ret);
+        ret
     }
 
     unsafe fn dump_to_file(&self, output: &str) {
-        let len = self.cursor.offset_from(self.start);
-        let slice = std::slice::from_raw_parts(self.start, len as usize);
+        let len = self.backing_memory_cursor.offset_from(self.backing_memory);
+        let slice = std::slice::from_raw_parts(self.backing_memory, len as usize);
         fs::write(output, slice).unwrap();
-    }
-
-    fn translate_to_target<T: Sized>(&self, host_ptr: *mut T) -> *mut T {
-        unsafe {
-            self.mem_base
-                .byte_offset(host_ptr.byte_offset_from(self.start)) as *mut T
-        }
     }
 }
 
@@ -80,4 +150,74 @@ pub fn dump_mem<O: ObjectModel>(_object_model: O, args: Args) -> Result<()> {
         memdump.dump_to_file(&memdump_args.output);
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_memif() -> MemdumpMemoryInterface {
+        let immix = MemdumpMapping {
+            backing_memory_start: 0x1_0000_0000usize as _,
+            target_memory_start: 0x8000_0000usize as _,
+            host_memory_start: 0x200_0000_0000usize as _,
+            size: 100usize * 1024 * 1024, // 100 MiB
+        };
+        let immortal = MemdumpMapping {
+            backing_memory_start: (0x1_0000_0000usize + 0x6400000) as _,
+            target_memory_start: (0x8000_0000usize + 0x6400000) as _,
+            host_memory_start: 0x400_0000_0000usize as _,
+            size: 100usize * 1024 * 1024, // 100 MiB
+        };
+        let los = MemdumpMapping {
+            backing_memory_start: (0x1_0000_0000usize + 0xc800000) as _,
+            target_memory_start: (0x8000_0000usize + 0xc800000) as _,
+            host_memory_start: 0x600_0000_0000usize as _,
+            size: 100usize * 1024 * 1024, // 100 MiB
+        };
+        let nonmoving = MemdumpMapping {
+            backing_memory_start: (0x1_0000_0000usize + 0x12c00000) as _,
+            target_memory_start: (0x8000_0000usize + 0x12c00000) as _,
+            host_memory_start: 0x800_0000_0000usize as _,
+            size: 100usize * 1024 * 1024, // 100 MiB
+        };
+        let mappings = vec![immix, immortal, los, nonmoving];
+        MemdumpMemoryInterface {
+            sorted_mappings: mappings,
+        }
+    }
+
+    #[test]
+    fn test_backing() {
+        let memif = get_memif();
+        assert_eq!(
+            memif.translate_to_backing(0x200_0000_0000usize as *const u8),
+            0x1_0000_0000usize as _
+        );
+        assert_eq!(
+            memif.translate_to_backing(0x200_0000_0008usize as *const u8),
+            0x1_0000_0008usize as _
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn test_backing_below_lower_bound() {
+        let memif = get_memif();
+        memif.translate_to_backing(0x100_0000_0000usize as *const u8);
+    }
+
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn test_backing_above_upper_bound() {
+        let memif = get_memif();
+        memif.translate_to_backing(0x900_0000_0000usize as *const u8);
+    }
+
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn test_backing_in_a_hole() {
+        let memif = get_memif();
+        memif.translate_to_backing(0x300_0000_0000usize as *const u8);
+    }
 }
